d48300f2d132404409efe2fdcf08d49f1fcead59
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-03-18 07:52:13 +0000

    Bringing Paths into Scope with the use Keyword

---
+++ The Rust Programming Language/07_04_scope.rs
@@ -0,0 +1,13 @@
+use crate::front_of_house::hosting::add_to_waitlist; 
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+
+pub fn eat_at_restaurant() {
+    add_to_waitlist();
+}
+
