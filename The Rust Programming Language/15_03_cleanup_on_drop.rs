a211717c02de19c1865406707b3e97f826882fa9
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-05-04 19:21:08 +0100

    Running Code on Cleanup with the Drop Trait

---
+++ The Rust Programming Language/15_03_cleanup_on_drop.rs
@@ -0,0 +1,32 @@
+struct CustomSmartPointer {
+    
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data: `{}`", self.data);
+    }
+}
+
+
+fn main() {
+    
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    
+    println!("CustomSmartPointers created.");
+    
+    let e = CustomSmartPointer {
+        data: String::from("some data"),
+    };
+    
+    println!("Another CustomSmartPointer created.");
+    drop(e);
+    println!("CustomSmartPointer dropped before the end of main.");
+}
