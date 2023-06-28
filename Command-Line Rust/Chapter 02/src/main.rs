efb00006f8f074c59bfe0cf3eaa4c492d911d0db
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-06-29 00:19:55 +0100

    Chapter 02 - Converted Arguments to Vec

--- Command-Line Rust/Chapter 02/src/main.rs
+++ Command-Line Rust/Chapter 02/src/main.rs
@@ -1,3 +1,12 @@
+extern crate getopts;
+use getopts::Options;
+use std::env;
+
+
 fn main() {
-    println!("{:?}", std::env::args());
+    
+    //Using https://docs.rs/getopts/latest/getopts/
+    
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 }
