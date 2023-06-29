7cdb5dde0f1ec2c2a4428fd3deb4f5de87204f97
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-06-29 19:05:00 +0100

    Chapter 02 - Added program help

--- Command-Line Rust/Chapter 02/src/main.rs
+++ Command-Line Rust/Chapter 02/src/main.rs
@@ -1,12 +1,26 @@
-extern crate getopts;
-use getopts::Options;
-use std::env;
+use clap::{App, Arg};
 
 
 fn main() {
     
-    //Using https://docs.rs/getopts/latest/getopts/
-    
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let arg_matches = App::new("test_for_echo")
+        .version("0.1.0")
+        .author("Ben Mouncer")
+        .about("Rust Echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1)
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false)
+        )
+        .get_matches();
+     
+     println!("{:#?}", arg_matches);                   
 }
