2903ab1731e68225e3081341ef4112249cf41ca1
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-06-30 22:43:29 +0100

    Test to check that valid args pass.

--- Command-Line Rust/Chapter 02/tests/cli.rs
+++ Command-Line Rust/Chapter 02/tests/cli.rs
@@ -10,3 +10,13 @@ fn dies_no_args() {
     // Check that it failed
     cmd.assert().failure().stderr(predicate::str::contains("USAGE"));
 }
+
+#[test]
+fn runs_with_args() {
+
+    // Call the program with valid arguments
+    let mut cmd = Command::cargo_bin("test_for_echo").unwrap();
+    
+    // Check that it passed with the argumen `hello`
+    cmd.arg("hello").assert().success();
+}
