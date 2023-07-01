981951e68568fb20f68b98a7ec84a6327eef30f7
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-07-01 16:53:45 +0100

    Formatted to allign with Rust style guidelines.

--- Command-Line Rust/Chapter 02/src/main.rs
+++ Command-Line Rust/Chapter 02/src/main.rs
@@ -1,8 +1,6 @@
 use clap::{App, Arg};
 
-
 fn main() {
-    
     /* Process the Programs Arguments */
     let arg_matches = App::new("test_for_echo")
         .version("0.1.0")
@@ -13,29 +11,27 @@ fn main() {
                 .value_name("TEXT")
                 .help("Input text")
                 .required(true)
-                .min_values(1)
+                .min_values(1),
         )
         .arg(
             Arg::with_name("omit_newline")
                 .short("n")
                 .help("Do not print newline")
-                .takes_value(false)
+                .takes_value(false),
         )
         .get_matches();
-     
-     /* Create Variables based on the Arguments */
-     let text = arg_matches.values_of_lossy("text").unwrap();
-     let omit_newline = arg_matches.is_present("omit_newline");
-     
-     /* Create the String to Print */
-     let output = text.join(" ");
-     
-     /* Print to STDOUT */
-     if omit_newline {
-        print!("{}", output)
-     
-     } else {
-        println!("{}", output)
-     }             
-}
 
+    /* Create Variables based on the Arguments */
+    let text = arg_matches.values_of_lossy("text").unwrap();
+    let omit_newline = arg_matches.is_present("omit_newline");
+
+    /* Create the String to Print */
+    let output = text.join(" ");
+
+    /* Print to STDOUT */
+    if omit_newline {
+        print!("{}", output)
+    } else {
+        println!("{}", output)
+    }
+}
