563fb3a939b8b2a712bbfe9924026f74ae3714c3
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-07-01 17:02:17 +0100

    Refactored test to return .

--- Command-Line Rust/Chapter 02/tests/cli.rs
+++ Command-Line Rust/Chapter 02/tests/cli.rs
@@ -2,6 +2,8 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 /* Echo output comparison files */
 const COMP_FILES: &[&str] = &[
     "tests/expected/echo_output_0.txt",
@@ -11,33 +13,39 @@ const COMP_FILES: &[&str] = &[
 ];
 
 #[test]
-fn dies_no_args() {
+fn dies_no_args() -> TestResult {
     /* Call the program without arguments */
-    let mut cmd = Command::cargo_bin("test_for_echo").unwrap();
+    let mut cmd = Command::cargo_bin("test_for_echo")?;
 
     /* Check that it failed */
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+    
+    return Ok(())
 }
 
 #[test]
-fn runs_with_args() {
+fn runs_with_args() -> TestResult  {
     /* Call the program with valid arguments */
-    let mut cmd = Command::cargo_bin("test_for_echo").unwrap();
+    let mut cmd = Command::cargo_bin("test_for_echo")?;
 
     /* Check that it passed with the argumen `hello` */
     cmd.arg("hello").assert().success();
+    
+    return Ok(())
 }
 
 #[test]
-fn match_echo_output_0() {
+fn match_echo_output_0() -> TestResult  {
     /* Read the test string from file */
-    let expected = fs::read_to_string(COMP_FILES[0]).unwrap();
+    let expected = fs::read_to_string(COMP_FILES[0])?;
 
     /* Run the program */
-    let mut cmd = Command::cargo_bin("test_for_echo").unwrap();
+    let mut cmd = Command::cargo_bin("test_for_echo")?;
 
     /* Give the program arguments and check the result */
     cmd.arg("Hello there").assert().success().stdout(expected);
+    
+    return Ok(())
 }
