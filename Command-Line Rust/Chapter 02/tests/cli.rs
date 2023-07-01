cdcd63e695048ba024c478bcce58fd40ef49bcd3
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-07-01 16:51:58 +0100

    Formatted to allign with Rust style guidelines.

--- Command-Line Rust/Chapter 02/tests/cli.rs
+++ Command-Line Rust/Chapter 02/tests/cli.rs
@@ -10,38 +10,34 @@ const COMP_FILES: &[&str] = &[
     "tests/expected/echo_output_3.txt",
 ];
 
-//const COMP_FILE_0: &str = "tests/expected/echo_output_0.txt";
-
-
 #[test]
 fn dies_no_args() {
-    
     /* Call the program without arguments */
     let mut cmd = Command::cargo_bin("test_for_echo").unwrap();
-    
+
     /* Check that it failed */
-    cmd.assert().failure().stderr(predicate::str::contains("USAGE"));
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
 }
 
 #[test]
 fn runs_with_args() {
-
     /* Call the program with valid arguments */
     let mut cmd = Command::cargo_bin("test_for_echo").unwrap();
-    
+
     /* Check that it passed with the argumen `hello` */
     cmd.arg("hello").assert().success();
 }
 
 #[test]
 fn match_echo_output_0() {
-    
     /* Read the test string from file */
     let expected = fs::read_to_string(COMP_FILES[0]).unwrap();
-    
+
     /* Run the program */
     let mut cmd = Command::cargo_bin("test_for_echo").unwrap();
-    
+
     /* Give the program arguments and check the result */
     cmd.arg("Hello there").assert().success().stdout(expected);
 }
