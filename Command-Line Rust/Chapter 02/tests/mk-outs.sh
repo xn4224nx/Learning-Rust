b48bbadd8faa62edef7675ff6329fac9ecc95078
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-07-01 15:27:58 +0100

    Bash script to create testing files.

---
+++ Command-Line Rust/Chapter 02/tests/mk-outs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# define the output directory
+OUTDIR="./expected"
+
+# Test if the output directory exists and create it if it doesn't exist
+[[ ! -d "$OUTDIR" ]] && mkdir -p "$OUTDIR"
+
+# Create files with text in them
+echo "Hello there" > $OUTDIR/echo_output_0.txt
+echo "Hello" "there" > $OUTDIR/echo_output_1.txt
+echo -n "Hello there" > $OUTDIR/echo_output_2.txt
+echo -n "Hello" "there" > $OUTDIR/echo_output_4.txt
+
