da5fcff8a73899dd2dd1f11eaf64e15b899931ce
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-07-17 19:51:26 +0100

    Added test constants and imports.

---
+++ Command-Line Rust/Chapter 03/tests/cli.rs
@@ -0,0 +1,12 @@
+//! Command Line Test Cat Tests
+
+use rand::{distributions::Alphanumeric, Rng};
+
+/// Test Constants
+const PRG: &str = "on_the_catwalk";
+const EMPTY: &str = "tests/inputs/empty.txt";
+const FOX: &str = "tests/inputs/fox.txt";
+const SPIDERS: &str = "tests/inputs/spiders.txt";
+const BUSTLE: &str = "tests/inputs/the-bustle.txt";
+
+
