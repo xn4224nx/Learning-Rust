4b79344e267258915b4f648a30cc565988ea1ce4
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-07-20 21:59:00 +0100

    Created missing filename function.

--- Command-Line Rust/Chapter 03/tests/cli.rs
+++ Command-Line Rust/Chapter 03/tests/cli.rs
@@ -10,3 +10,21 @@ const SPIDERS: &str = "tests/inputs/spiders.txt";
 const BUSTLE: &str = "tests/inputs/the-bustle.txt";
 
 
+fn  create_missing_filename() -> String {
+    
+    /* Loop until a bad file is found */
+    loop {
+        
+        /* Create a random 7 letter file name */
+        let filename: String = rand::thread_rng()
+                .sample_iter(&Alphanumeric)
+                .take(7)
+                .map(char::from)
+                .collect();
+        
+        /* If the file doesn't exist return the filename */     
+        if fs::metadata(&filename).is_err() {
+            return filename
+        }
+    }
+}
