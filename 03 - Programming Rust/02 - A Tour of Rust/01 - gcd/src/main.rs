e05338d11b1fee12117e893271bba1344bb9b4f1
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-11 21:08:40 +0100

    Expanded main to read numbers from the command line.

--- 03 - Programming Rust/02 - A Tour of Rust/01 - gcd/src/main.rs
+++ 03 - Programming Rust/02 - A Tour of Rust/01 - gcd/src/main.rs
@@ -1,3 +1,7 @@
+use std::str::FromStr;
+use std::env;
+
+
 fn gcd(mut a: u64, mut b: u64) -> u64 {
 
     /* Check the input variables */
@@ -15,5 +19,31 @@ fn gcd(mut a: u64, mut b: u64) -> u64 {
 }
 
 fn main() {
-    println!("{}", gcd(462, 1071));
+    
+    /* Vector to store the numbers from the command line. */
+    let mut nums = Vec::new();
+    
+    /* Read the command line arguments */
+    for arg in env::args().skip(1) {
+    
+        /* Append each argument to the nums vector. */
+        nums.push(
+            u64::from_str(&arg).expect("Error Parsing Argument")
+        );
+    }
+    
+    /* Check that enough numbers have been provided. */
+    if nums.len() < 2 {
+        eprintln!("Usage: gcd NUMBER NUMBER ...");
+        std::process::exit(1);
+    }
+    
+    /* Calculate the gcd of all the provided numbers */
+    let mut d = nums[0];
+    
+    for m in &nums[1..] {
+        d = gcd(d, *m);
+    }
+    
+    println!("The greatest common divisor of {:?} is {}", nums, d);
 }
