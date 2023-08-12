5b53d134bc7ce2c9bd8648081e9cd272b4057beb
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-12 17:28:29 +0100

    Added .

--- 03 - Programming Rust/02 - A Tour of Rust/02 - actix_gcd/src/main.rs
+++ 03 - Programming Rust/02 - A Tour of Rust/02 - actix_gcd/src/main.rs
@@ -1,3 +1,36 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+
 fn main() {
-    println!("Hello, world!");
+    
+    /* Create a new server object. */
+    let server = HttpServer::new(|| {
+        App::new().route("/", web::get().to(get_index))
+    });
+    
+    println!("Serving on http://localhost:3000...");
+    
+    /* Run the server. */
+    server.bind("127.0.0.1:3000")
+        .expect("Error binding server to address.")
+        .run()
+        .expect("Error running server.");
+}
+
+fn get_index() -> HttpResponse {
+    
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(
+            r#"
+                <title>GCD Calculator</title>
+                <form action="/gcd" method="post">
+                <input type="text" name="n"/>
+                <input type="text" name="m"/>
+                <button type="submit">Compute GCD</button>
+                </form>
+            "#,  
+        
+        )
+
+
 }
