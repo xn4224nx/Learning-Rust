63d1a808ac39f33af6d6af29171694be4b84d181
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-21 06:28:24 +0100

    escape time function.

--- 03 - Programming Rust/02 - A Tour of Rust/03 - mandelbrot/src/main.rs
+++ 03 - Programming Rust/02 - A Tour of Rust/03 - mandelbrot/src/main.rs
@@ -1,3 +1,22 @@
+use num::Complex;
+
+/// Determine if `c` is in the Mandelbrot set. 
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+   
+    let mut z = Complex {re: 0.0, im: 0.0};
+    
+    for i in 0..limit {
+        
+        /* Return the number of iterations that it took to prove it wasn't in */
+        if z.norm_sqr() > 4.0 {return Some(i);}
+        z = z * z + c;
+    }
+    
+    /* If it can't be proved to be outside the set. */
+    return None;
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", escape_time(Complex{re: 0.0, im: 0.0}, 20));
 }
