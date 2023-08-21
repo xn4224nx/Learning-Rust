543ac0da539e1f98937349f1b6fbac5f78967002
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-21 07:29:22 +0100

    Added parse res fn and moved fn's to lib.rs

---
+++ 03 - Programming Rust/02 - A Tour of Rust/03 - mandelbrot/src/lib.rs
@@ -0,0 +1,39 @@
+use num::Complex;
+use std::str::FromStr;
+
+/// Determine if `c` is in the Mandelbrot set.
+pub fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+
+    for i in 0..limit {
+        /* Return the number of iterations that it took to prove it wasn't in */
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+
+    /* If it can't be proved to be outside the set. */
+    return None;
+}
+
+/// Parse the commandline arguments that define image resolution.
+pub fn parse_img_res<T: FromStr>(raw_res: &str, separator: char) -> Option<(T, T)> {
+    /* Ensure the seperator exists in the res string. */
+    return match raw_res.find(separator) {
+        /* If it doesn't exist return None. */
+        None => None,
+
+        /* If it does parse the resolutions using the index of the sep. */
+        Some(index) => {
+            /* Try and extract the two halves of the resolution. */
+            match (
+                T::from_str(&raw_res[..index]),
+                T::from_str(&raw_res[index + 1..]),
+            ) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None,
+            }
+        }
+    };
+}
--- 03 - Programming Rust/02 - A Tour of Rust/03 - mandelbrot/src/main.rs
+++ 03 - Programming Rust/02 - A Tour of Rust/03 - mandelbrot/src/main.rs
@@ -1,23 +1,6 @@
+use mandelbrot::escape_time;
 use num::Complex;
 
-
 fn main() {
-    println!("{:?}", escape_time(Complex{re: 0.0, im: 0.0}, 20));
-}
-
-
-/// Determine if `c` is in the Mandelbrot set. 
-fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
-   
-    let mut z = Complex {re: 0.0, im: 0.0};
-    
-    for i in 0..limit {
-        
-        /* Return the number of iterations that it took to prove it wasn't in */
-        if z.norm_sqr() > 4.0 {return Some(i);}
-        z = z * z + c;
-    }
-    
-    /* If it can't be proved to be outside the set. */
-    return None;
+    println!("{:?}", escape_time(Complex { re: 0.0, im: 0.0 }, 20));
 }
