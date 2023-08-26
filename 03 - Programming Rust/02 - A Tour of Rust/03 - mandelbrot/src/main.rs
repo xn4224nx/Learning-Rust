cff0ce8152e3e446f0669c238b38cca8cb0bfccc
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-26 20:04:14 +0100

    Finished concurrent main.

--- 03 - Programming Rust/02 - A Tour of Rust/03 - mandelbrot/src/main.rs
+++ 03 - Programming Rust/02 - A Tour of Rust/03 - mandelbrot/src/main.rs
@@ -1,6 +1,30 @@
-use mandelbrot::escape_time;
-use num::Complex;
+use std::env;
 
 fn main() {
-    println!("{:?}", escape_time(Complex { re: 0.0, im: 0.0 }, 20));
+    /* Collect the program arguments. */
+    let args: Vec<String> = env::args().collect();
+
+    /* If not enough arguments are given print the correct usage. */
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
+        std::process::exit(1)
+    }
+
+    /* Extract the image resolution, upper_left and lower_right. */
+    let bounds = parse_img_res(&args[2], 'x');
+    let upper_left = parse_complex_res(&args[3]);
+    let lower_right = parse_complex_res(&args[4]);
+
+    /* Create the pixel array with all zeros. */
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    /* Fill the pixel array. */
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    /* Save the pixel array as an image. */
+    write_img(&args[1], &pixels, bounds).expect("Error writing PNG file.");
 }
