1bc894d22e16b6a2dcb291e10a4a2f9434bd54d1
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-30 06:50:31 +0100

    Type casts and some integer functions.

---
+++ 03 - Programming Rust/03 - Fundamental Types/02_type_casts/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    /* Conversions that are in range. */
+    assert_eq!(10_i8 as u16, 10_u16);
+    assert_eq!(2525_u16 as i16, 2525_i16);
+
+    assert_eq!(-1_i16 as i32, -1_i32);
+    assert_eq!(65535_u16 as i32, 65535_i32);
+
+    /* Conversions that truncate. */
+    assert_eq!(1000_i16 as u8, 232_u8);
+    assert_eq!(65535_u32 as i16, -1_i16);
+
+    assert_eq!(-1_i8 as u8, 255_u8);
+    assert_eq!(255_u8 as i8, -1_i8);
+
+    /* Some standard library operations for integers. */
+    assert_eq!(2_u16.pow(4), 16);
+    assert_eq!((-4_i32).abs(), 4);
+    assert_eq!(0b101101_u8.count_ones(), 4);
+    assert_eq!(0b101101_u8.count_zeros(), 4);
+    assert_eq!(0b101101_u8.leading_zeros(), 2);
+}
