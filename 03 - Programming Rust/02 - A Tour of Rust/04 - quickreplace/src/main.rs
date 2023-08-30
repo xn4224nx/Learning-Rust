0716e2e5cc73964f3e1f46c6cf1f3ea99d87f601
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-30 03:32:38 +0100

    Added argument parsing.

--- 03 - Programming Rust/02 - A Tour of Rust/04 - quickreplace/src/main.rs
+++ 03 - Programming Rust/02 - A Tour of Rust/04 - quickreplace/src/main.rs
@@ -1,3 +1,48 @@
+use std::env;
+use text_colorizer::*;
+
+#[derive(Debug)]
+struct Arguments {
+    target: String,
+    replacement: String,
+    filename: String,
+    output: String,
+}
+
+fn print_usage() {
+    eprintln!(
+        "{} - change occurances of one string into another",
+        "quickreplace".green()
+    );
+    eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
+}
+
+fn parse_arguments() -> Arguments {
+    /* Extract the arguments given to the program. */
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    /* Ensure the right number of arguments have been given. */
+    if args.len() != 4 {
+        print_usage();
+        eprintln!(
+            "{} wrong number of arguments: expected 4, got {}.",
+            "ERROR".red().bold(),
+            args.len()
+        );
+
+        std::process::exit(1);
+    }
+
+    /* Add the arguments into the structure and return. */
+    return Arguments {
+        target: args[0].clone(),
+        replacement: args[1].clone(),
+        filename: args[2].clone(),
+        output: args[3].clone(),
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = parse_args();
+    println!("{:?}", args);
 }
