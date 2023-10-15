2310f37ca1de9868b3dfe8b4144215148ab041f4
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-10-15 20:16:44 +0100

    Structure ownership example.

--- 03 - Programming Rust/04 - Ownership and Moves/00_Ownership/src/main.rs
+++ 03 - Programming Rust/04 - Ownership and Moves/00_Ownership/src/main.rs
@@ -1,7 +1,8 @@
+struct Person { name: String, birth: i32}
+
 fn main() {
     
     print_padovan();
-    
     
     {
         let point = Box::new((0.625, 0.5));     // point allocatedon the heap 
@@ -10,9 +11,19 @@ fn main() {
         
         assert_eq!(label, "(0.625, 0.5)");
         
-        println!("\npoint = '{:?}'\nlabel = '{}'", point, label);
+        println!("\npoint = '{:?}'\nlabel = '{}'\n", point, label);
         // Both label and point dropped here 
-    }    
+    }
+    
+    let mut composers = Vec::new();
+    
+    composers.push(Person {name: "Palestrina".to_string(), birth: 1525});
+    composers.push(Person {name: "Dowland".to_string(), birth: 1563});
+    composers.push(Person {name: "Lully".to_string(), birth: 1632});
+    
+    for comp in &composers {
+        println!("{}: born {}", comp.name, comp.birth);
+    }  
 }
 
 fn print_padovan() {
