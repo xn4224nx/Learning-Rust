825d8835031e4632c1ab595f329668ab843bee1a
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-10-15 21:32:33 +0100

    Moves and control flow.

---
+++ 03 - Programming Rust/04 - Ownership and Moves/02_control_flow/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    let x = vec![10, 20, 30];
+    let path_pick = true;
+    
+    if path_pick {
+        println!("x = {:?}", x);
+    
+    } else {
+        println!("x = {:?}", x);
+    }
+    
+    println!("x = {:?}", x);
+}
