ef3d16fe0c8f3f9ba1565642cf0aed4770520f8e
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-10-15 21:21:17 +0100

    Operations that move and moves without dropping.

--- 03 - Programming Rust/04 - Ownership and Moves/01_moves/src/main.rs
+++ 03 - Programming Rust/04 - Ownership and Moves/01_moves/src/main.rs
@@ -12,9 +12,24 @@ fn main() {
     let u = t;
     
     /* Show the contents of the noodle vector. */
-    println!("\n{:?}\n", u);
+    println!("\nu = {:?}\n", u);
     
     /* Can't use `noodles` or `u` here. */
     // println!("{:?}", t);
     // println!("{:?}", noodles);
+    
+    /* Operations That Move */
+    let mut name = "Govinda".to_string();
+    name = "Siddhartha".to_string();
+    
+    /* The "Govinda" has been overwritten. */
+    println!("name = {}\n", name);
+    
+    /* Move without dropping. */
+    let mut name2 = "Govinda".to_string();
+    let name2_t = name2;
+    name2 = "Siddhartha".to_string();
+    
+    println!("name2_t = {}\n", name2_t);
+    println!("name2 = {}\n", name2);
 }
