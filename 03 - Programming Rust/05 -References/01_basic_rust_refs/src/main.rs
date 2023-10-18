d83d27539d54e25eada7fa4b10283dda623e84f0
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-10-18 22:34:32 +0100

    Added toml file and struct refs.

--- 03 - Programming Rust/05 -References/01_basic_rust_refs/src/main.rs
+++ 03 - Programming Rust/05 -References/01_basic_rust_refs/src/main.rs
@@ -1,3 +1,9 @@
+struct Anime {
+
+    name: &'static str,
+    bechdel_pass: bool,
+}
+
 fn main() {
     
     /* x is a standard variable. */
@@ -17,4 +23,19 @@ fn main() {
     *m += 32;
     
     assert!(*m == 64);
+    
+    /* Create a structure instance and a ref to it. */
+    let aria = Anime{name: "Aria - The Animation", bechdel_pass: true};
+    let aria_ref = &aria;
+    
+    /* Derefencing happens automatically after a `.` */
+    println!("Name: '{}'\nPass: {}\n", aria_ref.name, aria_ref.bechdel_pass);
+    
+    /* The `.` operator can borrow to the left if needed. */
+    let mut v = vec![2, 3, 9, 1];
+    v.sort();
+    
+    /* The last line is equivilent to the one below. */
+    (&mut v).sort();
+    println!("Sorted Vector = {:?}\n", v);
 }
