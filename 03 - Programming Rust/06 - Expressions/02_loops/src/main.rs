b55b7eb7791ecba3162d20b36fcda88549d86501
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-10-28 20:44:26 +0100

    Ran Cargo Format

--- 03 - Programming Rust/06 - Expressions/02_loops/src/main.rs
+++ 03 - Programming Rust/06 - Expressions/02_loops/src/main.rs
@@ -1,58 +1,53 @@
 fn main() {
-    
     let mut i: i32 = 0;
-    
+
     /* While Loop */
     println!("\nWhile Loop");
-    
+
     while i < 4 {
         println!("\ti = {}", i);
         i += 1;
     }
-    
+
     /* While Let Loop */
     println!("\nWhile Let Loop");
     let mut opt_val = Some(0);
-    
+
     while let Some(n) = opt_val {
-    
-       if n > 9 {
-            println!("\tLoop exited."); 
+        if n > 9 {
+            println!("\tLoop exited.");
             opt_val = None;
-            
-       } else {
+        } else {
             println!("\tLoop value = {}", n);
-            opt_val = Some(n+1)
-       } 
+            opt_val = Some(n + 1)
+        }
     }
-    
+
     /* Loop Loop */
     println!("\nLoop Loop");
-    
+
     loop {
         println!("\ti = {}", i);
         i += 1;
-        if i > 10 {break};
+        if i > 10 {
+            break;
+        };
     }
-    
+
     /* For Loop */
     println!("\nFor Loop");
-    
+
     for j in 0..=7 {
         println!("\tj = {}", j);
     }
-    
+
     /* For loops consume the value unless refernced with & */
     let errors = vec!["404", "File not find", "File access restricted"];
-    
+
     println!("\nErrors:");
     for err in &errors {
         println!("\t{}", err);
     }
-    
+
     println!("\nNumber of errors: {}\n", errors.len())
-    
-    
-    
-    
 }
