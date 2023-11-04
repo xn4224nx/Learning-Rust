006522f479fa128094b03bced98cd1e20fe453ee
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-11-04 21:50:24 +0000

    Test for errors and sucess and extract both value types.

--- 03 - Programming Rust/07 - Error Handling/01_basic_error_handling/src/main.rs
+++ 03 - Programming Rust/07 - Error Handling/01_basic_error_handling/src/main.rs
@@ -1,13 +1,18 @@
 fn div_res(numer: i32, denom: i32) -> Result<i32, &'static str> {
-    
     /* Attempt the division. */
-    return numer.checked_div(denom).ok_or_else(
-        || {"division by zero"}
-    );
+    return numer.checked_div(denom).ok_or_else(|| "division by zero");
 }
 
 fn main() {
+    let top: i32 = 10;
+    let bot: i32 = 0;
 
-    /* Basic fn usage */
-    println!("{:?}", div_res(10, 5));
+    let calc = div_res(top, bot);
+
+    /* is_ok() and is_err() will not consume calc but  unwrap() will. */
+    if calc.is_ok() {
+        println!("{} / {} = {}", top, bot, calc.unwrap());
+    } else if calc.is_err() {
+        println!("{} / {} causes error '{}'", top, bot, calc.err().unwrap());
+    }
 }
