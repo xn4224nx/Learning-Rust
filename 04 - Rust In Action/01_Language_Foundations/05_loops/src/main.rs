c3c73f012c6d8308d529b6943664f9cd3975697d
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-12-04 02:07:55 +0000

    Rust for loop types.

---
+++ 04 - Rust In Action/01_Language_Foundations/05_loops/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    
+    /* A Basic For loop */
+    let my_vec0 = vec![1, 2, 3, 4, 5, 6, 7];
+    for item in my_vec0 {
+        println!("{}", item);
+    }
+    
+    /* Non consuming for loop. */
+    let my_vec1 = vec![1, 2, 3, 4, 5, 6, 7];
+    for item in &my_vec1 {
+        println!("{}", item);
+    }    
+    
+    /* Using the loop again. */
+    println!("my_vec1[0] = {}", my_vec1[0]);
+    
+    /* Mutate a container within loop. */
+    let mut my_vec2 = vec![1, 2, 3, 4, 5, 6, 7];
+    for item in &mut my_vec2 { *item = *item + 1; }
+    println!("my_vec2 = {:?}", my_vec2);
+}
