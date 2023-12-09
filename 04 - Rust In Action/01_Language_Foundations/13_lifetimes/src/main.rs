8e26f2404c1729e1114cd88451a2425498f67d67
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-12-09 17:56:05 +0000

    Basic lifetime usage.

---
+++ 04 - Rust In Action/01_Language_Foundations/13_lifetimes/src/main.rs
@@ -0,0 +1,12 @@
+fn add_with_lifetimes<'a, 'b>(i: &'a i32, j: &'b i32) -> i32 {
+
+    return *i + *j;
+}
+
+fn main() {
+    let a = 10;
+    let b = 20;
+    let res = add_with_lifetimes(&a, &b);
+    
+    println!("{} + {} = {}", a, b, res);
+}
