acab388b7b993b443b4cf476278bdd2d945608fb
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-12-17 17:32:39 +0000

    Changed line numbering to use enumeration.

--- 04 - Rust In Action/01_Language_Foundations/16_grep_lite/src/main.rs
+++ 04 - Rust In Action/01_Language_Foundations/16_grep_lite/src/main.rs
@@ -7,13 +7,9 @@ dark square is a picture feverishly turned--in search of what?
 It is the same with books.
 What do we seek through millions of pages?";
 
-    let mut line_num: usize = 1;
-
-    for line in quote.lines() {
+    for (i, line) in quote.lines().enumerate() {
         if line.contains(search_term) {
-            println!("{}: {}", line_num, line);
+            println!("{}: {}", i + 1, line);
         }
-
-        line_num += 1;
     }
 }
