e980749e1487c1bb613779c6fcd66b45efc089af
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-12-20 16:59:50 +0000

    Matching via regex.

--- 04 - Rust In Action/01_Language_Foundations/19_exact_str_matches/src/main.rs
+++ 04 - Rust In Action/01_Language_Foundations/19_exact_str_matches/src/main.rs
@@ -1,6 +1,10 @@
+use regex::Regex;
+
 fn main() {
     
     let search_term = "picture";
+    let re_search = Regex::new("picture").unwrap();
+    
     let quote = "
 Every face, every shop,
 bedroom window, public-house, and
@@ -13,8 +17,17 @@ What do we seek
 through millions of pages?";
 
     for (i, line) in quote.lines().enumerate() {
+        
+        /* Search using the contains function. */
         if line.contains(search_term) {
             println!("{}: {}", i, line);
+        }
+        
+        /* Search using regex. */
+        let contains_substr = re_search.find(line);
+        match contains_substr {
+            Some(extr) => println!("{}: {} {}", i, line, extr.as_str()),
+            None => (),
         }
     }
 }
