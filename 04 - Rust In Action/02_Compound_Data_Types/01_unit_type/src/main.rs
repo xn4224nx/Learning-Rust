a54dd54a91b5f0d4f4b44e4f6e626d44c12c158b
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-12-25 19:50:30 +0000

    Unit return type.

---
+++ 04 - Rust In Action/02_Compound_Data_Types/01_unit_type/src/main.rs
@@ -0,0 +1,20 @@
+use std::fmt::Debug;
+
+/* Implicitly return a unit type. */
+fn report<T: Debug>(item: &T) {
+    println!("{:?}", item);
+}
+
+/* Explicitly return a unit type. */
+fn clear(text: &mut String) -> () {
+
+    *text = String::from("");
+}
+
+fn main() {
+    let mut mystring = String::from("Test");
+    
+    report(&mystring);
+    clear(&mut mystring); 
+    report(&mystring);    
+}
