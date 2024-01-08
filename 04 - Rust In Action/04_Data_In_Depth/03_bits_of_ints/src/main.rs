f4f85a19d90dad43122ccfa08acb584b70b135f3
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-01-08 18:59:24 +0000

    Integers in bit form.

---
+++ 04 - Rust In Action/04_Data_In_Depth/03_bits_of_ints/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let two: u16 = 0b0000_0000_0000_0010;
+    let sixtyfivethousandfivehundredthirtyfive: u16 = 0b1111_1111_1111_1111;
+    
+    println!("{}, {}", two, sixtyfivethousandfivehundredthirtyfive);
+}
