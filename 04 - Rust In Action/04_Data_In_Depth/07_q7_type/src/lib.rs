567319b865a1f563b13dbb50a3d0b7a5db82fe86
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-01-12 21:41:26 +0000

    Moved structure and methods to library file.

---
+++ 04 - Rust In Action/04_Data_In_Depth/07_q7_type/src/lib.rs
@@ -0,0 +1,33 @@
+/* Q7 is a tuple stuct whose fields are not meant to be accessed. */
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Q7(pub i8);
+
+impl From<f64> for Q7 {
+    fn from(n: f64) -> Self {
+        if n >= 1.0 {
+            return Q7(127);
+        } else if n <= -1.0 {
+            return Q7(-128);
+        } else {
+            return Q7((n * 128.0) as i8);
+        }
+    }
+}
+
+impl From<f32> for Q7 {
+    fn from(n: f32) -> Self {
+        return Q7::from(n as f64);
+    }
+}
+
+impl From<Q7> for f64 {
+    fn from(n: Q7) -> f64 {
+        return (n.0 as f64) * 2_f64.powf(-7.0);
+    }
+}
+
+impl From<Q7> for f32 {
+    fn from(n: Q7) -> f32 {
+        return f64::from(n) as f32;
+    }
+}
