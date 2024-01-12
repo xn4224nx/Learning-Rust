b6aa689c22b923487fa0ebc84707e06bfda8fa85
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-01-12 20:58:55 +0000

    Implementation of the Q7 type.

---
+++ 04 - Rust In Action/04_Data_In_Depth/07_q7_type/src/main.rs
@@ -0,0 +1,29 @@
+/* Q7 is a tuple stuct whose fields are not meant to be accessed. */
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Q7(i8);
+
+impl From<f64> for Q7 {
+    fn from(n: f64) -> Self {
+        if n >= 1.0 {
+            return Q7(127);
+        } else if n <= -1.0 {
+            return Q7(-128);
+        } else {
+            return Q7((n * 128.0) as i8);
+        }
+    }
+}
+
+impl From<Q7> for f64 {
+    fn from(n: Q7) -> f64 {
+        return (n.0 as f64) * 2_f64.powf(-7.0);
+    }
+}
+
+fn main() {
+    let test_val = Q7(40);
+    let test_val2: f64 = 40.0;
+
+    println!("{:?} {}", test_val, f64::from(test_val));
+    println!("{} {:?}", test_val2, Q7::from(test_val2));
+}
