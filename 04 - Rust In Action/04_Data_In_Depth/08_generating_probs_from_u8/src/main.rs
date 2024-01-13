c24a041b3dfc234f28bd6b938b24f963f6b4919d
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-01-13 16:38:51 +0000

    Generation method not using division.

--- 04 - Rust In Action/04_Data_In_Depth/08_generating_probs_from_u8/src/main.rs
+++ 04 - Rust In Action/04_Data_In_Depth/08_generating_probs_from_u8/src/main.rs
@@ -4,9 +4,28 @@ fn mock_rand(n: u8) -> f32 {
 	return (n as f32) / 255.0;
 }
 
+fn mock_rand_fast(n: u8) -> f32 {
+	
+	let base: u32 = 0b0_01111110_00000000000000000000000;
+	
+	/* Aligns the input byte to 32 bits then increases its values by
+	 * shifting 15 places to the left. */
+	let large_n = (n as u32) << 15;
+	
+	/* Take a bitwise OR merging the base with the input byte. */
+	let f32_bits = base | large_n;
+	
+	/* Interprets an u32 as f32. */
+	let m = f32::from_bits(f32_bits);
+	
+	/* Normalises the output range of the value. */
+	return 2.0 * (m - 0.5);
+}
+
 fn main() {
     let sourced = random::<u8>();
     let prob = mock_rand(sourced);
+    let f_prob = mock_rand_fast(sourced);
     
-    println!("u8 = {}, f32 = {}", sourced, prob);
+    println!("u8 = {}, f32 = {}, fast_f32 = {}", sourced, prob, f_prob);
 }
