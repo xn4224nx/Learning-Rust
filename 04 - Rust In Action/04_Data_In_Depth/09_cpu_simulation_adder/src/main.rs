557ec0f37c8074009fc66a168a3375f82037d26b
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-01-16 23:28:15 +0000

    CPU methods, run, read and add.

--- 04 - Rust In Action/04_Data_In_Depth/09_cpu_simulation_adder/src/main.rs
+++ 04 - Rust In Action/04_Data_In_Depth/09_cpu_simulation_adder/src/main.rs
@@ -4,6 +4,32 @@ struct CPU {
     registers: [u8; 2],
 }
 
+impl CPU {
+    fn read_opcode(&self) -> u16 {
+        return self.current_operation;
+    }
+
+    fn run(&mut self) {
+        //loop {
+        let opcode = self.read_opcode();
+
+        let c = ((opcode & 0xF000) >> 12) as u8;
+        let x = ((opcode & 0x0F00) >> 8) as u8;
+        let y = ((opcode & 0x00F0) >> 4) as u8;
+        let d = ((opcode & 0x000F) >> 0) as u8;
+
+        match (c, x, y, d) {
+            (0x8, _, _, 0x4) => self.add_xy(x, y),
+            _ => todo!("opcode {:04x}", opcode),
+        }
+        //}
+    }
+
+    fn add_xy(&mut self, x: u8, y: u8) {
+        self.registers[x as usize] += self.registers[y as usize];
+    }
+}
+
 fn main() {
     /* Initialise the CPU with zeros. */
     let mut cpu = CPU {
