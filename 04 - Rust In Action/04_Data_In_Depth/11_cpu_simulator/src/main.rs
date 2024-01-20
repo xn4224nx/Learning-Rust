ac664fd12ac5d623e8e265cde4160798f547caa2
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-01-20 15:30:10 +0000

    CPU running multiple instructions.

--- 04 - Rust In Action/04_Data_In_Depth/11_cpu_simulator/src/main.rs
+++ 04 - Rust In Action/04_Data_In_Depth/11_cpu_simulator/src/main.rs
@@ -28,6 +28,60 @@ impl CPU {
             self.registers[0xF] = 0;
         }
     }
+
+    fn run(&mut self) {
+        loop {
+            let opcode = self.read_opcode();
+            self.prog_cntr += 2;
+
+            /* Decoding the op code. */
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = ((opcode & 0x000F) >> 0) as u8;
+
+            /* Short circuit the function to terminate execution
+             *  when 0x0000 is encountered. */
+            match (c, x, y, d) {
+                (0, 0, 0, 0) => {
+                    return;
+                }
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                _ => todo!("opcode {:04x}", opcode),
+            }
+        }
+    }
 }
 
-fn main() {}
+fn main() {
+    let mut cpu = CPU {
+        registers: [0; 16],
+        memory: [0; 4096],
+        prog_cntr: 0,
+    };
+
+    /* Initialise the registers. */
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+    cpu.registers[2] = 10;
+    cpu.registers[3] = 10;
+
+    let mem = &mut cpu.memory;
+
+    /* Add register 1 to register 0, via opcode 0x8014 */
+    mem[0] = 0x80;
+    mem[1] = 0x14;
+
+    /* Add register 2 to register 0, via opcode 0x8024 */
+    mem[2] = 0x80;
+    mem[3] = 0x24;
+
+    /* Add register 3 to register 0, via opcode 0x8034 */
+    mem[4] = 0x80;
+    mem[5] = 0x34;
+
+    cpu.run();
+
+    assert_eq!(cpu.registers[0], 35);
+    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
+}
