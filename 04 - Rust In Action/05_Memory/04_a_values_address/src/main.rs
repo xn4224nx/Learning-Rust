2ffa1a2d03386591d4e41e850c4c3c076d04f13f
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-02-25 21:25:45 +0000

    Dereferencing pointers.

--- 04 - Rust In Action/05_Memory/04_a_values_address/src/main.rs
+++ 04 - Rust In Action/05_Memory/04_a_values_address/src/main.rs
@@ -9,4 +9,12 @@ fn main() {
     };
 
     println!("a: {} ({:p}...0x{:x})", a, a_ptr, a_addr + 7);
+
+    /* You can create pointers safely from integers */
+    let ptr = 42 as *const Vec<String>;
+
+    unsafe {
+        let new_addr = ptr.offset(4);
+        println!("{:p} -> {:p}", ptr, new_addr);
+    }
 }
