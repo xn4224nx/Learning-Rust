d1f27ea76286db2f9d808275988d15a85b17b6f8
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-02-26 17:21:06 +0000

    Adapting fns to use different string types.

---
+++ 04 - Rust In Action/06_accpting_different_str_types/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    /* A string stored on the stack. */
+    let heap_str = "password123";
+
+    /* A string stored on the heap. */
+    let stack_str = String::from("mypassword");
+
+    println!("Is '{}' strong? - {}", &heap_str, is_strong(&heap_str));
+    println!("Is '{}' strong? - {}", &stack_str, is_strong(&stack_str));
+}
+
+fn is_strong<T: AsRef<str>>(password: T) -> bool {
+    return password.as_ref().len() > 5;
+}
