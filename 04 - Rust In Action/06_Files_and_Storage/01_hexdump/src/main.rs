3bc159ac3061555678dee34a35ffd7cb1207a80a
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-04-09 19:44:00 +0100

    Cargo Format

--- 04 - Rust In Action/06_Files_and_Storage/01_hexdump/src/main.rs
+++ 04 - Rust In Action/06_Files_and_Storage/01_hexdump/src/main.rs
@@ -7,20 +7,19 @@ fn main() {
 }"#;
 
 fn main() -> std::io::Result<()> {
-    
     /* Make space for the program's input. */
     let mut buffer: Vec<u8> = vec![];
-    
+
     /* Read the input into the buffer. */
     INPUT.read_to_end(&mut buffer)?;
-    
-    for (idx, line) in buffer.chunks(BYTES_PER_LINE).enumerate()  {
-		print!("\n[0x{:08x}] ", idx);
-		for byte in line {
-			print!("{:02x} ", byte);
-		}
-	}
-	
-	println!("\n\n");
-	return Ok(());
+
+    for (idx, line) in buffer.chunks(BYTES_PER_LINE).enumerate() {
+        print!("\n[0x{:08x}] ", idx);
+        for byte in line {
+            print!("{:02x} ", byte);
+        }
+    }
+
+    println!("\n\n");
+    return Ok(());
 }
