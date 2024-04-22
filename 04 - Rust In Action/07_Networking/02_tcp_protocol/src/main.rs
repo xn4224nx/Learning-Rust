069ccac2c75d326cb5285710d9f0f479f678b081
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-04-22 17:33:23 +0100

    Refactored into function.

--- 04 - Rust In Action/07_Networking/02_tcp_protocol/src/main.rs
+++ 04 - Rust In Action/07_Networking/02_tcp_protocol/src/main.rs
@@ -1,17 +1,20 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 
-fn main() -> std::io::Result<()> {
-    let host = String::from("www.bbc.co.uk");
+fn main() {
+    crude_get("www.bbc.co.uk").expect("Connection failed!");
+}
+
+fn crude_get<S: AsRef<str> + std::fmt::Display>(url: S) -> std::io::Result<()> {
+    /* Construct the connection details. */
     let port = String::from("80");
-    let conn_details_0 = format!("{host}:{port}");
-    let conn_details_1 = format!("Host: {host}");
+    let conn_details_0 = format!("{url}:{port}");
+    let conn_details_1 = format!("Host: {url}");
 
+    /* Connect to the host. */
     let mut conn = TcpStream::connect(conn_details_0)?;
-
     conn.write_all(b"GET / HTTP/1.0")?;
     conn.write_all(b"\r\n")?;
-
     conn.write_all(conn_details_1.as_bytes())?;
     conn.write_all(b"\r\n\r\n")?;
 
