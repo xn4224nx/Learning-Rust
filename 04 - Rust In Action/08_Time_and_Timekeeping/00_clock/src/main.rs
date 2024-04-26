47ae3bb71d2265257f0b364fc2724772763d3b68
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-04-26 13:51:03 +0100

    Added different formats.

--- 04 - Rust In Action/08_Time_and_Timekeeping/00_clock/src/main.rs
+++ 04 - Rust In Action/08_Time_and_Timekeeping/00_clock/src/main.rs
@@ -1,8 +1,48 @@
-use chrono::Local;
+use chrono::{DateTime, Local};
+use clap::{App, Arg};
+
+struct Clock;
+
+impl Clock {
+    fn get() -> DateTime<Local> {
+        return Local::now();
+    }
+
+    fn set() -> ! {
+        unimplemented!()
+    }
+}
 
 fn main() {
-    /* Time in the system's local time. */
-    let now = Local::now();
-    
-    println!("{}", now);
+    let app = App::new("clock")
+        .version("0.1")
+        .about("Gets the time.")
+        .arg(
+            Arg::with_name("action")
+                .takes_value(true)
+                .possible_values(&["get"])
+                .default_value("get"),
+        )
+        .arg(
+            Arg::with_name("std")
+                .short("s")
+                .long("use-standard")
+                .takes_value(true)
+                .possible_values(&["rfc2822", "rfc3339", "timestamp"])
+                .default_value("rfc3339"),
+        );
+
+    let args = app.get_matches();
+
+    let action = args.values_of("action").unwrap();
+    let std = args.value_of("std").unwrap();
+
+
+    let now = Clock::get();
+    match std {
+        "timestamp" => println!("{}", now.timestamp()),
+        "rfc2822" => println!("{}", now.to_rfc2822()),
+        "rfc3339" => println!("{}", now.to_rfc3339()),
+        _ => unreachable!(),
+    }
 }
