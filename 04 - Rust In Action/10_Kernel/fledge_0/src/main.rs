ad3a186d453f47d7768b730c3e8daacee3c9a525
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-05-04 16:13:46 +0100

    Print text in the OS.

--- 04 - Rust In Action/10_Kernel/fledge_0/src/main.rs
+++ 04 - Rust In Action/10_Kernel/fledge_0/src/main.rs
@@ -4,12 +4,60 @@
 #![feature(lang_items)]
 /* Unlock the LLVM compiler's intrinsic functions. */
 #![feature(core_intrinsics)]
-use core::intrinsics;
 
+use core::intrinsics;
 /* Allow the panic handler to inspect where the error occured. */
 use core::panic::PanicInfo;
-
 use x86_64::instructions::hlt;
+
+#[allow(unused)]
+#[derive(Clone, Copy)]
+#[repr(u8)]
+enum Colour {
+    Black = 0x0,
+    White = 0xF,
+    Blue = 0x1,
+    BrightBlue = 0x9,
+    Green = 0x2,
+    BrightGreen = 0xA,
+    Cyan = 0x3,
+    BrightCyan = 0xB,
+    Red = 0x4,
+    BrightRed = 0xC,
+    Magenta = 0x5,
+    BrightMagenta = 0xD,
+    Brown = 0x6,
+    Yellow = 0xE,
+    Gray = 0x7,
+    DarkGrey = 0x8,
+}
+
+struct Cursor {
+    position: isize,
+    foreground: Colour,
+    background: Colour,
+}
+
+impl Cursor {
+    fn colour(&self) -> u8 {
+        let fg = self.foreground as u8;
+        let bg = (self.background as u8) << 4;
+        return fg | bg;
+    }
+
+    fn print(&mut self, text: &[u8]) {
+        let colour = self.colour();
+        let framebuffer = 0xb8000 as *mut u8;
+
+        for &char in text {
+            unsafe {
+                framebuffer.offset(self.position).write_volatile(char);
+                framebuffer.offset(self.position + 1).write_volatile(colour);
+            }
+            self.position += 2;
+        }
+    }
+}
 
 #[panic_handler]
 #[no_mangle]
@@ -25,12 +73,14 @@ pub extern "C" fn eh_personality() {}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let framebuffer = 0xb8000 as *mut u8;
+    let text = b"Hello, world!";
 
-    unsafe {
-        /* Increment the pointer by one and set the background to cyan */
-        framebuffer.offset(1).write_volatile(0x30);
-    }
+    let mut cur = Cursor {
+        position: 0,
+        foreground: Colour::BrightBlue,
+        background: Colour::Yellow,
+    };
+    cur.print(text);
 
     loop {
         hlt();
