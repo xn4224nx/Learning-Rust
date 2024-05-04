cf232891507266a703f278f5fb64d96a3cf9680b
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-05-04 14:32:41 +0100

    OS code.

--- 04 - Rust In Action/10_Kernel/fledge_0/src/main.rs
+++ 04 - Rust In Action/10_Kernel/fledge_0/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+/* Prepare for not having as OS. */
+#![no_std]
+#![no_main]
+
+/* Unlock the LLVM compiler's intrinsic functions. */
+#![feature(core_intrinsics)]
+use core::instrinsics;
+
+/* Allow the panic handler to inspect where the error occured. */
+use core::panic::PanicInfo;
+
+#[panic_handler]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    intrinsics::abort();
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let framebuffer = 0xb8000 as *mut u8;
+    
+    unsafe {
+        /* Increment the pointer by one and set the background to cyan */
+        framebuffer.offset(1).write_volatile(0x30);
+    }
+    
+    loop {}
 }
