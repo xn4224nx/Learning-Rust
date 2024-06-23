04218ad06814bef0c46ff8417b22e220fe45aeb6
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-06-23 01:30:27 +0100

    Added multi-line messages and limit the chars that can be printed.

--- 05 - Practical Rust Projects/02_Building_a_Command_Line_Program/src/main.rs
+++ 05 - Practical Rust Projects/02_Building_a_Command_Line_Program/src/main.rs
@@ -9,6 +9,9 @@
  * digital ecclesiastical veneer.
  */
 
+const MAX_MSG_LINE_CHAR: usize = 50;
+const NUM_MSG_LINES: usize = 5;
+
 use std::env::args;
 
 fn main() {
@@ -16,12 +19,24 @@ fn main() {
         .nth(1)
         .unwrap_or(String::from("That would be an ecumenical matter!"));
 
+    let mut msg_split: Vec<String> = vec![String::new(); NUM_MSG_LINES];
+
+    /* Split the message into MAX_MESSAGE_LINE sized chunks. */
+    for (idx, ms_char) in message.chars().enumerate() {
+        /* Don't print charcters beyond the limit of the program */
+        if idx >= MAX_MSG_LINE_CHAR * NUM_MSG_LINES {
+            break;
+        }
+
+        msg_split[idx / MAX_MSG_LINE_CHAR].push(ms_char);
+    }
+
     println!(
         r#"
                __
          /\   |  \
         /  \  |  /
-       |    | |        {}
+       |    | |        {}                                       
        \____/ |       /{}   
        ( o o) |      / {}
        /\__/\ |   __/  {}
@@ -35,6 +50,6 @@ fn main() {
      |  |  | /|
      '''''''' |
     "#,
-        message, "", "", "", ""
+        msg_split[0], msg_split[1], msg_split[2], msg_split[3], msg_split[4]
     );
 }
