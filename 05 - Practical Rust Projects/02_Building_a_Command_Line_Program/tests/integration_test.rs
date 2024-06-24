4fd0b96a667cdf094e8c5252570a6e069ebccc3c
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-06-24 05:03:26 +0100

    Further program tests.

--- 05 - Practical Rust Projects/02_Building_a_Command_Line_Program/tests/integration_test.rs
+++ 05 - Practical Rust Projects/02_Building_a_Command_Line_Program/tests/integration_test.rs
@@ -12,3 +12,53 @@ fn run_with_defaults() {
             "That would be an ecumenical matter!",
         ));
 }
+
+#[test]
+fn run_with_short_options() {
+    Command::cargo_bin("robo_preacher")
+        .expect("binary exists")
+        .args(&["-e", "-c"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("^ ^"));
+}
+
+#[test]
+fn run_with_long_options() {
+    Command::cargo_bin("robo_preacher")
+        .expect("binary exists")
+        .args(&["--eyes", "--colors"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("^ ^"));
+}
+
+#[test]
+fn run_with_txt_input() {
+    Command::cargo_bin("robo_preacher")
+        .expect("binary exists")
+        .args(&["test string with spaces"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("test string with spaces"));
+}
+
+#[test]
+fn run_with_small_txt_input() {
+    Command::cargo_bin("robo_preacher")
+        .expect("binary exists")
+        .args(&["test_string_without_spaces"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("test_string_without_spaces"));
+}
+
+#[test]
+fn run_with_very_long_input() {
+    Command::cargo_bin("robo_preacher")
+        .expect("binary exists")
+        .args(&["a".repeat(4000)])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("a"));
+}
