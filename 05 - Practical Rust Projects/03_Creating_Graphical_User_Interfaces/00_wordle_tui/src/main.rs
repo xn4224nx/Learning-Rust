2492f9f29a71205dfdfdc802b42aa2269d0ba0a5
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-06-25 22:46:46 +0100

    Added quit on esc key.

--- 05 - Practical Rust Projects/03_Creating_Graphical_User_Interfaces/00_wordle_tui/src/main.rs
+++ 05 - Practical Rust Projects/03_Creating_Graphical_User_Interfaces/00_wordle_tui/src/main.rs
@@ -1,19 +1,23 @@
 /*
  * --- Text User Interface Wordle ---
- * 
+ *
  * An implementation of the word guessing game Wordle. The game is
- * works through a text user interface using the ncurses package. 
- */ 
+ * works through a text user interface using the ncurses package.
+ */
 
-use cursive::{self, views::TextView};
+use cursive::event::Key;
+use cursive::views::TextView;
+use cursive::{self};
 
 fn main() {
-    
     let mut siv = cursive::default();
-    
+
     /* Add content to the app. */
     siv.add_layer(TextView::new("Hello world!"));
-    
+
+    /* Pressing the esc key exits the program. */
+    siv.add_global_callback(Key::Esc, |x| x.quit());
+
     /* Start the event loop. */
     siv.run();
 }
