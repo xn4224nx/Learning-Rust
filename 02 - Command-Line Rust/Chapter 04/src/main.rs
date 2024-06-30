04683254650cb695f3240130c1e7de36b9bdb79c
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-06-30 21:29:54 +0100

    Basic command line parsing.

--- 02 - Command-Line Rust/Chapter 04/src/main.rs
+++ 02 - Command-Line Rust/Chapter 04/src/main.rs
@@ -1,3 +1,28 @@
+/*
+ * +++ Head +++
+ *
+ * Print the top lines or bytes of a file.
+ *
+ */
+
+use clap::{Command, Parser};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Options {
+    /// File to read the top lines or bytes of.
+    file: PathBuf,
+
+    #[clap(short = 'c', long = "bytes")]
+    /// How many bytes of the file would you like to read?
+    nbytes: Option<usize>,
+
+    #[clap(short = 'n', long = "lines")]
+    /// How many lines of the file would you like to read?
+    nlines: Option<usize>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = Options::parse();
 }
