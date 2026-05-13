17b1ffddcbf4ee88e82ae66158257e615815c522
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2026-05-13 08:50:29 +0100

    Obstacle spawning.

--- 06 - Bevy First Steps/src/main.rs
+++ 06 - Bevy First Steps/src/main.rs
@@ -4,11 +4,23 @@
  */
 
 use bevy::prelude::*;
+use bevy_prng::WyRand;
+use bevy_rand::prelude::{EntropyPlugin, GlobalRng};
+use rand_core::Rng;
+
+const GAME_SPEED: f32 = 400.0;
 
 const GROUND_LEVEL: f32 = -100.0;
+const GROUND_SIZE: Vec2 = Vec2::new(30.0, 50.0);
+const GROUND_EDGE: f32 = GROUND_SIZE.x / 2.0;
+
 const PLAYER_X: f32 = -300.0;
 const JUMP_FORCE: f32 = 600.0;
 const GRAVITY: f32 = -800.0;
+const SPAWN_INTERVAL: f32 = 1.0;
+
+const OBSTACLE_SIZE: Vec2 = Vec2::new(30.0, 30.0);
+const OBSTACLE_COLOR: Color = Color::srgb(1.0, 0.0, 0.0);
 
 #[derive(Component)]
 struct Player;
@@ -16,11 +28,31 @@ struct Player;
 #[derive(Component)]
 struct Velocity(Vec3);
 
+#[derive(Component)]
+struct Obstacle;
+
+#[derive(Resource)]
+struct ObstacleSpawningTimer(Timer);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(EntropyPlugin::<WyRand>::default())
         .add_systems(Startup, setup)
-        .add_systems(Update, (jump, apply_gravity_to_player, player_movement))
+        .add_systems(
+            Update,
+            (
+                jump,
+                apply_gravity_to_player,
+                player_movement,
+                spawn_obstacles,
+                move_obstacles,
+            ),
+        )
+        .insert_resource(ObstacleSpawningTimer(Timer::from_seconds(
+            SPAWN_INTERVAL,
+            TimerMode::Repeating,
+        )))
         .run();
 }
 
@@ -83,5 +115,47 @@ fn player_movement(
 fn apply_gravity_to_player(time: Res<Time>, mut query: Query<&mut Velocity, With<Player>>) {
     for mut vel in query.iter_mut() {
         vel.0.y += GRAVITY * time.delta_secs();
+    }
+}
+
+/// Randomly place obstacles for the player to avoid
+fn spawn_obstacles(
+    mut cmds: Commands,
+    time: Res<Time>,
+    mut spawn_timer: ResMut<ObstacleSpawningTimer>,
+    mut rng: Single<&mut WyRand, With<GlobalRng>>,
+) {
+    /* Advance the timer. */
+    spawn_timer.0.tick(time.delta());
+
+    if spawn_timer.0.is_finished() {
+        let obst_x = GROUND_EDGE;
+        let obst_y = GROUND_LEVEL + (rng.next_u32() % 50) as f32;
+
+        cmds.spawn((
+            Obstacle,
+            Sprite {
+                color: OBSTACLE_COLOR,
+                custom_size: Some(OBSTACLE_SIZE),
+                ..default()
+            },
+            Transform::from_xyz(obst_x, obst_y, 0.0),
+        ));
+    }
+}
+
+/// Move towards the player and remove them after they are off-screen.
+fn move_obstacles(
+    time: Res<Time>,
+    mut cmds: Commands,
+    mut query: Query<(Entity, &mut Transform), With<Obstacle>>,
+) {
+    for (ent_obj, mut transform) in query.iter_mut() {
+        transform.translation.x -= GAME_SPEED * time.delta_secs();
+
+        /* Remove obstacles that have disapeared off-screen. */
+        if transform.translation.x < -GROUND_EDGE {
+            cmds.entity(ent_obj).despawn();
+        }
     }
 }
