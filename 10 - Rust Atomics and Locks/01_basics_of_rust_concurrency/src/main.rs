e2c73985ea2b662a60d845a1114c6d23bad02ed5
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2026-07-19 12:32:04 +0100

    Scoped threads & reference counting.

--- 10 - Rust Atomics and Locks/01_basics_of_rust_concurrency/src/main.rs
+++ 10 - Rust Atomics and Locks/01_basics_of_rust_concurrency/src/main.rs
@@ -3,6 +3,7 @@
  * =====================================
  */
 
+use std::sync::Arc;
 use std::thread;
 
 fn main() {
@@ -14,6 +15,42 @@ fn main() {
     /* Ensure the program does not finish until the threads have completed. */
     tr1.join().unwrap();
     tr2.join().unwrap();
+
+    /* Scoped Threads */
+    let numbers = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    thread::scope(|s| {
+        s.spawn(|| {
+            println!(
+                "Thread: {:?} - Length: {}",
+                thread::current().id(),
+                numbers.len()
+            );
+        });
+        s.spawn(|| {
+            for num in numbers.iter() {
+                println!("{num:}");
+            }
+        });
+    });
+
+    /* Reference Counting */
+    let a = Arc::new([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    let b = a.clone();
+
+    thread::spawn(move || dbg!(a));
+    thread::spawn(move || dbg!(b));
+
+    /* Internal Scope Names */
+    let c = Arc::new([
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    ]);
+    thread::spawn({
+        let c = c.clone();
+        move || {
+            dbg!(c);
+        }
+    });
+    dbg!(c);
 }
 
 fn f() {
