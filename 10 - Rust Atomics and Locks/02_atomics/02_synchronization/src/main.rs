c99ab37734d85d598411c7cdd3f44f72b0654a58
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2026-07-21 11:38:06 +0100

    park_timeout

---
+++ 10 - Rust Atomics and Locks/02_atomics/02_synchronization/src/main.rs
@@ -0,0 +1,66 @@
+/*
+ * Chapter 2. Atomics
+ * ==================
+ *
+ * Every time the background thread finishes processing an item, it stores the
+ * number of processed items in an AtomicUsize. Meanwhile, the main thread shows
+ * that number to the user to inform them of the progress, about once per
+ * second.
+ */
+
+use std::io::Write;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering::Relaxed;
+use std::thread::{current, park_timeout, scope, sleep};
+use std::time::Duration;
+
+const NUM_TASKS: usize = 100;
+const MAX_LOADING_DOTS: usize = 5;
+
+fn main() {
+    let num_done = AtomicUsize::new(0);
+    let main_thrd = current();
+
+    scope(|s| {
+        let mut num_loading_dots = 0;
+
+        /* A background thread to process all 100 items. */
+        s.spawn(|| {
+            for task_idx in 1..=NUM_TASKS {
+                sleep(Duration::from_secs(1));
+                num_done.store(task_idx, Relaxed);
+
+                /* Wake up the main thread */
+                main_thrd.unpark();
+            }
+        });
+
+        /* The main thread to show status updates. */
+        loop {
+            num_loading_dots = num_loading_dots % MAX_LOADING_DOTS + 1;
+            let curr_progress = num_done.load(Relaxed);
+
+            /* Show the progress to STDOUT */
+            print!(
+                "\rWorking {curr_progress:}/{NUM_TASKS:} {}{}",
+                std::iter::repeat('.')
+                    .take(num_loading_dots)
+                    .collect::<String>(),
+                std::iter::repeat(' ')
+                    .take(MAX_LOADING_DOTS - num_loading_dots)
+                    .collect::<String>()
+            );
+            std::io::stdout().flush().unwrap();
+
+            /* Clean up the progress bar. */
+            if curr_progress == NUM_TASKS {
+                print!("\rAll tasks completed successfully.\n");
+                std::io::stdout().flush().unwrap();
+                break;
+            }
+
+            /* Put this thread into a holding state until the other thread wakes it. */
+            park_timeout(Duration::from_millis(500));
+        }
+    });
+}
